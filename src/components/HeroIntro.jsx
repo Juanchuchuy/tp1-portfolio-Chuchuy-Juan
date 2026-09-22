fb4dcdb6fa656a9a867a57f73240d51a3ee2d552
Juanchuchuy <juanignaciolinares2013@gmail.com>
2026-09-22 15:56:17 -0300

    Se añadio el componente de perfil principal

--- src/components/HeroIntro.jsx
+++ src/components/HeroIntro.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './HeroIntro.css'
+import { useTypewriter } from "typify-react";
+import {ChevronRight} from 'lucide-react'
+
+import profileImage from '../assets/profile-image.jpg'
 
 const HeroIntro = () => {
+
+   const [career] = useTypewriter({
+    words: ["Software Developer", "UX/UI Designer", "Student From UTN"],
+    loop: 0, 
+    typeSpeed: 100,
+    deleteSpeed: 50,
+    delaySpeed: 1500,
+  });
+
+
+
   return (
-    <div>heroIntro</div>
+    <div className='hero-container'>
+      <div className='profile'>
+            <div className='profile-title'>
+              <h2>Juan</h2>
+              <h2>Chuchuy</h2>
+
+              <div className='profile-career'><ChevronRight className='career-arrow' /> {career} <p className='cursor'>_</p></div>
+            </div>
+            <div className='profile-img'>
+                <img src={profileImage} alt="Juan Chuchuy self image" />
+            </div>
+      </div>
+
+    </div>
   )
 }
 
